f421dc32dabb285baa6839eaeaccabe9a30f9b66
everythxxgs <leochriba@gmail.com>
2023-11-24 08:48:38 +0100

    main

--- riddle/js/riddle.js
+++ riddle/js/riddle.js
@@ -1,7 +1,12 @@
 const myCanvas = document.getElementById("myCanvas");
 const parent = document.getElementById("par");
+var progress;
+
 const ctx = myCanvas.getContext("2d");
+
+
 ctx.fillStyle = "red";
+ctx.imageSmoothingEnabled = false;
 myCanvas.width = parent.offsetWidth;
 myCanvas.height = parent.offsetHeight;
 ctx.translate(myCanvas.width/2, myCanvas.height/2);
@@ -9,14 +14,24 @@ ctx.translate(myCanvas.width/2, myCanvas.height/2);
 if (!localStorage.getItem("progress")) {
     localStorage.setItem("progress", 1);
 }
-ctx.beginPath();
-for (let i = 1; i <= localStorage.getItem("progress"); i++) {
+else {
+    progress = localStorage.getItem("progress");
+}
+const img = new Image();
+img.src = "./images/document.png";
+console.log(img);
+var obj;
+img.onload = () =>{
+    obj = ctx.createImageData(16,16,);
+    ctx.putImageData(obj, -8, -8);
+    //ctx.drawImage(img, -8,-8);
 
 }
-ctx.stroke();
 
 
-onresize(e => {
+
+
+document.addEventListener("resize", e => {
     myCanvas.width = parent.offsetWidth;
     myCanvas.height = parent.offsetHeight;
     ctx.translate(myCanvas.width/2, myCanvas.height/2);
